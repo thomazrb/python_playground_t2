3cc6f72bc0eb7c335de681895fd39211bede821b
Thomaz Botelho <thomazrb@gmail.com>
2024-03-28 20:29:37 -0300

    Básico de saída de informação no Python

---
+++ saida1.py
@@ -0,0 +1,16 @@
+ddd = "aaa"
+bbb = 3
+
+print("ddd")
+print('ddd')
+print(ddd)
+print(bbb)
+
+print('ddd', ddd, 'bbb', bbb)
+
+x = 5
+print('O valor de x é:', x)
+
+nova_string = 'O valor de x é:', x
+print(type("O valor de x é 3"))
+print(type(nova_string))
