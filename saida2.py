444469dbcbed6bc541f1879221783628d82dd079
Thomaz Botelho <thomazrb@gmail.com>
2024-03-28 20:47:03 -0300

    Strings formatadas

---
+++ saida2.py
@@ -0,0 +1,17 @@
+# Saída com strings formatadas
+
+nome = "Joaozinho"
+idade = 15
+
+print("Nome:", nome , "Idade", 15)
+out = "Nome:", nome , "Idade", 15
+print(type(out))
+
+print("Nome: {} Idade {}".format(nome, idade))
+out2 = "Nome: {} Idade {}".format(nome, idade)
+print(type(out2))
+
+print("Nome: {0} Idade {1}, Mas a idade do {0} é {1}, ou seria {1}?".format(nome, idade))
+
+
+
