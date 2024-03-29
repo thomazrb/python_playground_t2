07877b411cb7ef6a8a802abd604822104d5cfd30
Thomaz Botelho <thomazrb@gmail.com>
2024-03-28 22:02:18 -0300

    condições

---
+++ condicoes.py
@@ -0,0 +1,21 @@
+x = 5
+
+if x == 3:
+    print('x é 3!')
+
+if x == 4:
+    print('x é 4!')
+
+if x != 3 and x != 4:
+    print('x não é 3 nem 4')
+
+
+####
+# Performance! 
+        
+if x == 3:
+    print('x é 3!')
+elif x == 4:
+    print('x é 4!')
+else:
+    print('x não é 3 nem 4')
