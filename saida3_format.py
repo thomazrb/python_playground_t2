3737bcab1e663133a4b61f39dfa19e8ac5c197f0
Thomaz Botelho <thomazrb@gmail.com>
2024-03-28 21:12:13 -0300

    formatando numeros e textos

---
+++ saida3_format.py
@@ -0,0 +1,15 @@
+x = 3.14159
+y = 4*x
+print("Os valores são:")
+print("{:8.2f}".format(x))
+print("{:8.2f}".format(y))
+
+
+
+x = "Teste de Nome"
+y = "menor"
+print("Os valores são:")
+print(f"{20*'x'}")
+print("{:^20}".format(x))
+print("{:^20}".format(y))
+print(f"{20*'x'}")
